e2325ef427a45af71fec6e39b1cb855036ab293f
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-01-29 23:53:05 +0000

    Fixed bug in rotation function

--- src/dataset/data-preprocessing/data transformations/data_transformer.py
+++ src/dataset/data-preprocessing/data transformations/data_transformer.py
@@ -51,7 +51,6 @@ def rotate(hand_gesture):
 	dotproduct = sum((a*b) for a, b in zip((0,10), p_9))
 
 	degrees = math.acos(dotproduct / 100)
-	print(degrees)
 	#degrees = degrees * math.pi / 180
 
 	#rotate it by that much 
@@ -62,7 +61,7 @@ def rotate(hand_gesture):
 		x[i] = math.cos(degrees) * x[i] - math.sin(degrees) * y[i]
 		y[i] = math.sin(degrees) * tmp + math.cos(degrees) * y[i]
 		#add x and y to list 
-		hand_gesture.extend([x, y])
+		hand_gesture.extend([x[i], y[i]])
 		i+=1
 	return hand_gesture
 
@@ -75,19 +74,26 @@ def transform_dataset():
 
 		with open('transformed_dataset.csv', 'w', newline='') as transformed_dataset:
 			csv_writer = csv.writer(transformed_dataset)
+			csv_writer.writerow(next(csv_reader))
+			firstline = True
 			for line in csv_reader:
+				if firstline:    #skip first line
+					firstline = False
+					continue
 				#convert coords from string to floats
-				line = line[0] + [float(item) for item in line[1:]]
+				line = [line[0]] + [float(item) for item in line[1:]]
 				#translate coordinates
 				line = translate(line)
 				#enlargement 
+				line = enlarge(line)
 				#rotation
-				#csv_writer.writerow(line)
+				line = rotate(line)
+				csv_writer.writerow(line)
 
 
 
 if __name__ == '__main__':					
-	#transform_dataset()
+	transform_dataset()
 	
 	############ AD HOC TRANSLATION TESTS ###############
 	#test_object = ['palm_108_keypoints', 550.552, 2280.4, 621.377, 2209.57, 678.713, 2125.25, 722.557, 2013.96, 800.128, 1943.13, 638.241, 1990.35, 634.868, 1852.07, 614.632, 1761.01, 587.651, 1673.32, 564.042, 1986.98, 550.552, 1828.46, 543.806, 1720.54, 523.57, 1632.85, 499.962, 1986.98, 496.589, 1835.21, 489.844, 1747.52, 476.353, 1666.57, 439.254, 2003.84, 419.018, 1889.17, 419.018, 1828.46, 419.018, 1757.63]
@@ -102,8 +108,8 @@ if __name__ == '__main__':
 	#plot(test_object)
 
 	############### AD HOC ROTATION TESTS ###############
-	test_object = ['palm_108_keypoints', 0.0, 0.0, 2.411228507145167, -2.411398731536776, 4.363225650606873, -5.282062871635336, 5.855889295750152, -9.070917380074244, 8.496784552054532, -11.48231611161102, 2.985361335164879, -9.87471695725317, 2.8705279605852723, -14.582442731598862, 2.1815958028642757, -17.682569351586704, 1.2630309408623868, -20.667964731629908, 0.45926540856178333, -9.989448197197811, 0.0, -15.386242308777788, -0.22966674915921353, -19.060365577272055, -0.9185989068802104, -22.045760957315256, -1.7223303943024924, -9.989448197197811, -1.837163768882099, -15.156439380105287, -2.0667964731629906, -18.141834760148488, -2.526095926603096, -20.89776766030241, -3.789126867465483, -9.415451548691387, -4.47805902518648, -13.319377745858155, -4.47805902518648, -15.386242308777788, -4.47805902518648, -17.797641040314563]
+	#test_object = ['palm_108_keypoints', 0.0, 0.0, 2.411228507145167, -2.411398731536776, 4.363225650606873, -5.282062871635336, 5.855889295750152, -9.070917380074244, 8.496784552054532, -11.48231611161102, 2.985361335164879, -9.87471695725317, 2.8705279605852723, -14.582442731598862, 2.1815958028642757, -17.682569351586704, 1.2630309408623868, -20.667964731629908, 0.45926540856178333, -9.989448197197811, 0.0, -15.386242308777788, -0.22966674915921353, -19.060365577272055, -0.9185989068802104, -22.045760957315256, -1.7223303943024924, -9.989448197197811, -1.837163768882099, -15.156439380105287, -2.0667964731629906, -18.141834760148488, -2.526095926603096, -20.89776766030241, -3.789126867465483, -9.415451548691387, -4.47805902518648, -13.319377745858155, -4.47805902518648, -15.386242308777788, -4.47805902518648, -17.797641040314563]
 	#plot(test_object)
-	test_object = rotate(test_object)
+	#test_object = rotate(test_object)
 	#print(test_object)
-	plot(test_object)
+	#plot(test_object)
