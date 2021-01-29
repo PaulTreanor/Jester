90334f84f83c2ffe15227779ea9ce81de7de7729
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-01-29 20:24:13 +0000

    Translation processing function finished, with all tests passed

--- src/dataset/data-preprocessing/data transformations/data_transformer.py
+++ src/dataset/data-preprocessing/data transformations/data_transformer.py
@@ -1,21 +1,22 @@
 import csv
-
+from gesture_plotter import plot
 
 def translate(hand_gesture):
 	x_dif = hand_gesture[1]
 	y_dif = hand_gesture[2]
-	x = gesture[1::2] 
-	y = gesture[2::2]
+	x = hand_gesture[1::2] 
+	y = hand_gesture[2::2]
 
 	hand_gesture = [hand_gesture[0]]
-	i = 0 
+	i = 0
 	while i < len(x):
-		#do x thing 
-		#add x to new list
-		#do y thing 
-		#add y to new list 
-	print(hand_gesture)
-	
+		x[i] = round(x[i] - x_dif, 3)
+		
+		hand_gesture.append(x[i])
+		y[i] = round(y[i] - y_dif, 3)
+		
+		hand_gesture.append(y[i])
+		i += 1
 
 	return hand_gesture
 
@@ -38,4 +39,10 @@ def transform_dataset():
 
 
 if __name__ == '__main__':					
-	transform_dataset()
+	#transform_dataset()
+	test_object = ['palm_108_keypoints', 550.552, 2280.4, 621.377, 2209.57, 678.713, 2125.25, 722.557, 2013.96, 800.128, 1943.13, 638.241, 1990.35, 634.868, 1852.07, 614.632, 1761.01, 587.651, 1673.32, 564.042, 1986.98, 550.552, 1828.46, 543.806, 1720.54, 523.57, 1632.85, 499.962, 1986.98, 496.589, 1835.21, 489.844, 1747.52, 476.353, 1666.57, 439.254, 2003.84, 419.018, 1889.17, 419.018, 1828.46, 419.018, 1757.63]
+	
+	########### AD HOC TRANSLATION TESTS #############
+	test_object = (translate(test_object))
+	print(test_object)
+	plot(test_object)
--- src/dataset/data-preprocessing/data transformations/data_transformer_tests.py
+++ src/dataset/data-preprocessing/data transformations/data_transformer_tests.py
@@ -5,26 +5,37 @@ from data_transformer import translate
 ############################### TRANSLATION UNIT TESTS ###################################
 
 @pytest.fixture
-def translated_coords():
-	test_object = ['palm_108_keypoints', 550.552, 2280.4, 621.377, 2209.57, 678.713, 2125.25, 722.557, 2013.96, 800.128, 1943.13, 638.241, 1990.35, 634.868, 1852.07, 614.632, 1761.01, 587.651, 1673.32, 564.042, 1986.98, 550.552, 1828.46, 543.806, 1720.54, 523.57, 1632.85, 499.962, 1986.98, 496.589, 1835.21, 489.844, 1747.52, 476.353, 1666.57, 439.254, 2003.84, 419.018, 1889.17, 419.018, 1828.46, 419.018, 1757.63]
-	return translated_coords
+def test_object():
+	test_object = ['palm_108_keypoints', 550.552, 2280.4, 621.377, 2209.57, 678.713, 2125.25, 722.557, 2013.96, 800.128, 1943.13, 638.241, 1990.35, 634.868, 1852.07, 614.632, 1761.01, 587.651, 1673.32, 564.042, 1986.98, 550.552, 1828.46, 543.806, 1720.54, 523.57, 1632.85, 499.962, 1986.98, 496.589, 1835.21, 489.844, 1747.52, 476.353, 1666.57, 439.254, 2003.84, 419.018, 1889.17, 419.018, 1828.46, 419.018, 1757.63]	
+	return test_object
 
 @pytest.mark.translations
-def test_translations_point_0(translated_coords):
+def test_translations_point_0(test_object):
+	translated_coords = translate(test_object)
+	x = translated_coords[1::2] 
+	y = translated_coords[2::2]
+
 	#point_0_x and point_0_y must = 0   
-	assert all(coords == 0 for coords in translated_coords[1:3])
+
+	assert [x[0], y[0]] == [0,0]
 
 
 	
-#@pytest.mark.translations
-def test_translations_point_12_pass(translated_coords):
+@pytest.mark.translations
+def test_translations_point_12_pass(test_object):
 	#check x and y of point 12 
-	assert translated_coords[37:39] == [26.982, -647.55]
+	translated_coords = translate(test_object)
+	x = translated_coords[1::2] 
+	y = translated_coords[2::2]
+	assert [x[12], y[12]] == [-26.982, -647.55]
 	
-#@pytest.mark.translations
-def test_translations_point_15_fail(translated_coords):
+@pytest.mark.translations
+def test_translations_point_15_fail(test_object):
+	translated_coords = translate(test_object)
+	x = translated_coords[1::2] 
+	y = translated_coords[2::2]
 	#coords of translated point_15 should be (-60.708, -532.88)
-	assert translated_coords[46:48] == [239.39, -337.85]
+	assert [x[15], y[15]] == [-60.708, -532.88]
 
 
 ############################### ENLARGEMENT UNIT TESTS ###################################
