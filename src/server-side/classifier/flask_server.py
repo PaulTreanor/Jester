5cf434cb7bda2ca7a924c432001c53cbf5f2ade8
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-02-26 13:39:47 +0000

    API can now handle requests from multiple devices concurrently

--- src/server-side/classifier/flask_server.py
+++ src/server-side/classifier/flask_server.py
@@ -4,20 +4,21 @@ from flask import Flask, request, abort
 from get_class import getClass
 
 api = Flask(__name__)
-upload_path = 'C:\\Users\\trean\\Desktop\\College\\4YP\\2021-ca400-ptreanor-cgorman\\src\\server-side\\classifier\\uploads'
+root_upload_path = 'C:\\Users\\trean\\Desktop\\College\\4YP\\2021-ca400-ptreanor-cgorman\\src\\server-side\\classifier\\uploads'
 
 @api.route("/<filename>", methods=["POST"])
 def post_file(filename):
-	"""Upload a file."""
-
-	if "/" in filename:
-		# Return 400 BAD REQUEST
-		print(filename)
-		abort(400, "no subdirectories allowed")
+	# Create new directory for each request (OpenPose only reads images)
+	filename, fileExtension = os.path.splitext(filename)
+	upload_path = root_upload_path + '/' + filename	
+	os.mkdir(upload_path)
 
 	file = request.files['file']
-	file.save('./uploads/cool.png')
+	file.save(upload_path +'/cool.png')
 	classification = getClass(upload_path)
+
+	# Delete directory
+	os.rmdir(upload_path)
 	# 201 CREATED
 	return classification, 201
 
