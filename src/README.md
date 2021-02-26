519959e98eeecf17cb38dee98851963b73bad74f
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-02-26 15:41:57 +0000

    Joined both server test files

--- src/README.md
+++ src/README.md
@@ -1,3 +1,2 @@
 All of your source code (and other program resources) should be placed in this sub-directory.
 
-Editing this as part of the test commit 
--- src/server-side/server_test/server_tests.py
+++ src/server-side/server_test/server_tests.py
@@ -1,8 +1,14 @@
 import os
-import requester#
 import requests
 
 API_URL = 'http://192.168.43.219:5000/'
+
+def post_file(filename):
+	files = {'file': open(filename, 'rb')}
+	r = requests.post(API_URL + filename, files=files)
+	print(r.status_code)
+	print(r.text)
+	return r
 
 def test_connection():
 	r = requests.get(API_URL)
@@ -10,15 +16,23 @@ def test_connection():
 
 def test_ood_image():
 	filename = 'test_image1.jpg'
-	r = requester.post_file(filename)
+	r = post_file(filename)
 	assert r.status_code == 201 and r.text == "OOD"
 
 def test_gesture_image():
 	filename = 'test_image2.jpg'
-	r = requester.post_file(filename)
+	r = post_file(filename)
 	assert r.status_code == 201 and r.text == "palm"
 
 def test_400_error():
 	filename = 'test_text.txt'
-	r = requester.post_file(filename)
+	r = post_file(filename)
 	assert r.status_code == 422
+
+if __name__ == "__main__":
+	# Test connection
+	r = requests.get(API_URL)
+	print(r.status_code, r.text)
+	# Send file
+	filename = 'test_image1.jpg'
+	post_file(filename)
