3af05dcee4d4b009890d5b4a8038902ea7cd2028
Paul Treanor <paul.treanor5@mail.dcu.ie>
2021-03-06 19:32:29 +0000

    Created vue.js app, started layout

---
+++ src/webApp/vue-webapp/jester/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Nav/>
+  </div>
+</template>
+
+<script>
+import Nav from './components/Nav';
+export default {
+  name: 'App',
+  components: {
+    Nav
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif; /* this is a nice font */
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+</style>
---
+++ src/webApp/vue-webapp/jester/src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Jester</b-navbar-brand>
+
+        
+            <!---nav items could be disabled when you're in that specific section --->
+            <!--- text is just placeholder for icons _---->
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item >Camera</b-nav-item>
+                <b-nav-item >Gallery</b-nav-item>
+                <b-nav-item >Info</b-nav-item>
+            </b-navbar-nav>
+
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Nav"
+   
+}
+</script>
+
+<style scoped>
+
+</style>
