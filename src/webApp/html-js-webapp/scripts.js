644f8f5ae16641eea3afc10ff52d2ce597c74500
Paul Treanor <treanorpaul9@gmail.com>
2021-03-25 17:59:59 +0000

    Can stop videos with hand gestures in html/js prototype app

--- src/webApp/html-js-webapp/scripts.js
+++ src/webApp/html-js-webapp/scripts.js
@@ -1,19 +1,22 @@
 let mediaRecorder;
 let recordedBlobs;
-let vidLength = 3000;
-
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+let vidLength = 20000;
 
 const recordedVideo = document.querySelector('video#recorded');
 const recordButton = document.querySelector('button#record');
-const snapButton = document.querySelector('button#snap');
 const preview = document.querySelector('video#preview');
 const playButton = document.querySelector('button#play');
 const downloadButton = document.querySelector('button#download');
 
+var controls = document.getElementById('controls');
+const pauseButton = document.querySelector('button#pause');
+const resumeButton = document.querySelector('button#resume');
+
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
 var post_url = "http://192.168.43.105:5000/image";
 // -------------------- UI FUNCTIONS -------------------//
+
 
 // Start/stop video
 recordButton.addEventListener('click', () => {
@@ -24,21 +27,27 @@ recordButton.addEventListener('click', () => {
   }
 });
 
-// Take screenshot/photo
-snapButton.addEventListener("click", function() {
-  takePhoto();
+// Pause/Result Video 
+pauseButton.addEventListener('click', () => {
+  pauseMedia();
 });
 
-function takePhoto(){
-  context.drawImage(preview, 0, 0, 640, 480);
-}
+// Pause/Result Video 
+resumeButton.addEventListener('click', () => {
+  resumeMedia();
+});
+
 
 playButton.addEventListener('click', () => {
+  playVideo();
+});
+
+function playVideo() {
   const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
   recordedVideo.src = window.URL.createObjectURL(superBuffer);
   recordedVideo.controls = true;
   recordedVideo.play();
-});
+}
 
 downloadButton.addEventListener('click', () => {
   const blob = new Blob(recordedBlobs, {type: 'video/webm'});
@@ -63,7 +72,7 @@ function handleDataAvailable(event) {
   }
 }
 
-function startRecording(vidLength) {
+function startRecording() {
   recordedBlobs = [];
   mediaRecorder = new MediaRecorder(window.stream, {mimeType: 'video/webm'});
   recordButton.textContent = 'Stop Recording';
@@ -72,8 +81,10 @@ function startRecording(vidLength) {
   mediaRecorder.ondataavailable = handleDataAvailable;
   mediaRecorder.start();
   setTimeout(function(){
-    stopRecording()
-    }, vidLength); 
+      checkVideoStop();
+
+
+  }, vidLength);  
 }
 
 function stopRecording() {
@@ -83,10 +94,95 @@ function stopRecording() {
   downloadButton.disabled = false;
 }
 
+
+function pauseMedia() {
+  mediaRecorder.requestData();
+  mediaRecorder.pause();
+  //play();
+}
+
+function resumeMedia() {
+  mediaRecorder.resume();
+  setTimeout(function(){
+      checkVideoStop();
+
+
+  }, vidLength); 
+}
+
+
+function checkVideoStop() {
+  pauseMedia();
+  //check handgesture 
+  updateServer();
+    // palm =
+}
+
+
+
+
+// -------------- API STUFF --------------//
+
+
+function takePhoto(){
+     context.drawImage(preview, 0, 0, 640, 480);
+}
+
+
+
+ // Send screenshots to server
+async function postData(post_url, image) {
+    let formData = new FormData();
+    formData.append('file', image);
+    console.log(image);
+
+    const response = await fetch(post_url, {
+    method: 'POST',
+    body: formData
+    });
+
+    response.text().then(function (text) {
+        console.log(text)
+        alert(text);
+        if (text =="palm"){
+          stopRecording();
+        }
+        if (text == "peace"){
+          takePhoto();
+        }
+        if (text == "thumbs_up"){
+          startRecording(vidLength);
+        }
+        
+        resumeMedia();
+        
+    });
+}
+
+function updateServer(){
+    takePhoto();
+    canvas.toBlob(function(blob){
+            //I can porbably remove this variable right?
+            var image = blob;
+
+            // emit blob for storage in gallery
+            postData(post_url, image)
+        }, 'image/jpeg', 0.95)
+}
+// Take and send photo every x seconds
+window.setInterval(updateServer, 20000);
+
+
+
+
+
+
+// --------------- MAIN ---------------------//
+
 // Access camera and display preview at id='preview'
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   // {audo: true} to get audio
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(function(stream) {
     recordButton.disabled = false;
     window.stream = stream;
     // Play video stream in preview box
@@ -94,46 +190,6 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
   });
 }
 
-// Send screenshots to server
-async function postData(post_url, image) {
-	let formData = new FormData();
-	formData.append('file', image);
-	console.log(image);
-
-    const response = await fetch(post_url, {
-      method: 'POST',
-      body: formData
-    });
-
-    response.text().then(function (text) {
-        console.log(text)
-        alert(text);
-        if (text =="palm"){
-          setTimeout(
-            takePhoto()
-            , 5000);
-        }
-        if (text == "peace"){
-          takePhoto();
-        }
-        if (text = "thumbs_up"){
-          startRecording(vidLength);
-        }
-        //else do nothing
-      });
-  }
-
-  
-function updateServer(){
-  takePhoto();
-  canvas.toBlob(function(blob){
-		var image = blob;
-		postData(post_url, image)
-	}, 'image/jpeg', 0.95)
-}
-
-// Take and send photo every x seconds
-window.setInterval(updateServer, 20000);
 
 
 
