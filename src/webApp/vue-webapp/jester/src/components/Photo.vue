e3259378161435fa0afcd9e4b2140e28289d594e
Paul Treanor <treanorpaul9@gmail.com>
2021-03-27 15:52:51 +0000

    Gallery download photo buttons working

--- src/webApp/vue-webapp/jester/src/components/Photo.vue
+++ src/webApp/vue-webapp/jester/src/components/Photo.vue
@@ -2,7 +2,7 @@
     <div class="mb-5">
         <b-img class="pb-3" v-bind:src='image_src' id="image" fluid ></b-img>
         <br>
-        <b-button class="mb-2" id="downloadPhoto" ><b-icon class="mr-2" icon="file-earmark-arrow-down"></b-icon>Download</b-button>
+        <b-button class="mb-2 downloadPhoto" v-on:click='downloadPhoto'><b-icon class="mr-2" icon="file-earmark-arrow-down"></b-icon>Download</b-button>
     </div>
 </template>
 
@@ -15,13 +15,9 @@ export default {
 			image_src : URL.createObjectURL(this.photo)
 		}
 	},
-    mounted() {
-		const downloadButton = document.querySelector('button#downloadPhoto');
-		let photo = this.photo;
-		
-
-		downloadButton.addEventListener('click', () => {
-            const blob = photo;
+    methods: {
+        downloadPhoto() {
+            const blob = this.photo;
             const url = window.URL.createObjectURL(blob);
             const a = document.createElement('a');
             a.style.display = 'none';
@@ -33,13 +29,15 @@ export default {
                 document.body.removeChild(a);
                 window.URL.revokeObjectURL(url);
             }, 100);
-            });
-	}
+        }
+    }
+
+    
 }
 </script>
 
 <style scoped>
-    #downloadPhoto {
+    .downloadPhoto {
         max-width: 640px;
         background-color: purple;
         border-color: purple;
